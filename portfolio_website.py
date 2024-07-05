0d8d5bba611bb6cd2ff8a4c08593601a1915b2b0
steveh <healeysa@gmail.com>
2024-07-05 13:18:10 -0400

    Add files via upload

--- portfolio_website.py
+++ portfolio_website.py
@@ -19,7 +19,7 @@ persona = """
         computer science from Wentworth Institute of Technology in Boston MA. He also later studied Project Management
         at Northeastern University. Steve also dabbles with Python and OpenCV (see video below) and has programmed Drones
         to fly autonomously. His favorite Drone is the DJI Air 3 and also has the Mavic Air 2. He lives north of 
-        Boston MA, USA. Steve's age is somewhat private, between 50 and 65. He has the mind of a 30 year old in a slightly 
+        Boston MA, USA. He is between 50 and 65 and he has the mind of a 30 year old in a slightly 
         older body, LOL. He has blue eyes, which is his favorite color. He weighs just under 175. This
         year he started riding a bicycle and absolutely loves it. Last week he road over 60 miles.
 
