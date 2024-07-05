75b59c8aa215f74fbe507b40a32ad1ead55c9b6a
steveh <healeysa@gmail.com>
2024-07-05 19:30:32 -0400

    Add files via upload

--- portfolio_website.py
+++ portfolio_website.py
@@ -8,12 +8,11 @@ model = genai.GenerativeModel('gemini-1.5-flash')
 col1, col2 = st.columns(2)
 
 with col1:
-    st.write(" ")
     st.subheader("Hello :wave:")
     st.title("I am Steven Healey")
 
 with col2:
-    st.image("images/steve3.png")
+    st.image("images/steve4.png")
 
 st.title(" ")
 
